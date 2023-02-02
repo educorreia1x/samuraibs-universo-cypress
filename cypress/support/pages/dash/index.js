cbd33e31893f88b83518ab1d942c08d2795b2f21
educorreia1x <63937188+educorreia1x@users.noreply.github.com>
2023-02-02 20:25:49 -0300

    Implementacoes do DashBoard, criando customer e provider

--- cypress/support/pages/dash/index.js
+++ cypress/support/pages/dash/index.js
@@ -1,10 +1,30 @@
-import {el} from './elements'
+import { el } from './elements'
 import header from '../../components/header'
 
 class DashPage {
 
     constructor() {
         this.header = header
+    }
+
+    calendarShouldBeVisible() {
+        cy.get('.DayPicker', { timeout: 7000 })
+            .should('be.visible')
+    }
+
+    selectDay(day) {
+        const target = new RegExp('^' + day + '$', 'g')
+        cy.contains('.DayPicker-Day--available', target)
+            .click({ force: true })
+    }
+
+    appointmentShouldBe(customer, hour) {
+        cy.contains('div', customer.name)
+            .should('be.visible')
+            .parent()
+            .contains('span[class=appointment]', hour)
+            .should('be.visible')
+
     }
 
 }
