522f9ecb71cb56d182e41fb3a110e33028249327
educorreia1x <63937188+educorreia1x@users.noreply.github.com>
2023-02-02 22:36:14 -0300

    Reimplementacao da massa de teste da feature do DashBoard usando a camada de fabricacao (factories/dash)

---
+++ cypress/support/factories/dash/index.js
@@ -0,0 +1,19 @@
+
+
+exports.customer = {
+    name: "Test Mobile",
+    email: "testmobile@putsbox.com",
+    password: "pwd123",
+    is_provider: false
+}
+
+exports.provider = {
+    name: "Provider Mobile",
+    email: "qa10@putsbox.com",
+    password: "pwd123",
+    is_provider: true
+}
+
+exports.appointment = {
+    hour: '14:00'
+}
